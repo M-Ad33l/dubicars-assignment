222d6d562beb8d90cb4d24125361e52ed9e3f929
M. Adeel <m.adeel@specialisedjobs.com>
2020-10-28 17:13:23 +0500

    Refactor Sorting Algo

--- src/BoardSorter.php
+++ src/BoardSorter.php
@@ -1,64 +1,97 @@
 <?php
 
-
 namespace TourSorter;
 
+use TourSorter\Cards\BoardCard;
 
 class BoardSorter
 {
-
     /**
-     * BoardSorter constructor.
      * @param array $boardCards
-     * @return array
+     * @return BoardCard[]
      * @throws SortingFailure
+     * @return BoardCard[]
      */
-
-    public function __construct()
-    {
-    }
-
     public function sort(array $boardCards): array
     {
-        return $boardCards;
         if (count($boardCards) <= 1) {
             return $boardCards;
         }
 
-        $dataStructure = [];
-        foreach ($boardCards as $boardCard) {
-            $origin = $boardCard['origin'];
-            $destination = $boardCard['destination'];
-            $dataStructure[$origin] = $destination;
-        }
+        $dataStructure = $this->extractDataStructure($boardCards);
 
         $origins = array_keys($dataStructure);
-        $destinations = array_values($dataStructure);
+        $destinations = array_column($dataStructure, 'destination');
 
+        $startingPoint = $this->getStartingPoint($origins, $destinations);
+        $finalDestination = $this->getFinalDestination($destinations, $origins);
+
+        return $this->sortList($startingPoint, $finalDestination, $dataStructure);
+    }
+
+    /**
+     * @param BoardCard[] $boardCards
+     * @return array
+     */
+    private function extractDataStructure(array $boardCards): array
+    {
+        $dataStructure = [];
+        foreach ($boardCards as $boardCard) {
+            $origin = $boardCard->getOrigin();
+            $destination = $boardCard->getDestination();
+            $dataStructure[$origin] =
+                [
+                    'destination' => $destination,
+                    'board' => $boardCard
+                ];
+        }
+        return $dataStructure;
+    }
+
+    /**
+     * @param $origins
+     * @param $destinations
+     * @return string
+     * @throws SortingFailure
+     */
+    private function getStartingPoint($origins, $destinations): string
+    {
         $startingPointDiff = array_diff($origins, $destinations);
         if (1 !== count($startingPointDiff)) {
-            throw new SortingFailure('Unable to get the starting point');
+            throw new SortingFailure('Error calculating tour');
         }
         $startingPoint = array_shift($startingPointDiff);
+        return $startingPoint;
+    }
 
+    /**
+     * @param $destinations
+     * @param $origins
+     * @return string
+     * @throws SortingFailure
+     */
+    private function getFinalDestination($destinations, $origins): string
+    {
         $finalDestinationDiff = array_diff($destinations, $origins);
-        if (1 !== count($finalDestinationDiff)) {
-            throw new SortingFailure('Unable to get the final destination point');
-        }
         $finalDestination = array_shift($finalDestinationDiff);
+        return $finalDestination;
+    }
 
+    /**
+     * @param $startingPoint
+     * @param $finalDestination
+     * @param $dataStructure
+     * @return BoardCard[]
+     */
+    private function sortList($startingPoint, $finalDestination, $dataStructure): array
+    {
         $currentDestination = $startingPoint;
         $sortedCards = [];
         while ($currentDestination != $finalDestination) {
-            $currentDestination = $dataStructure[$startingPoint];
-            $sortedCards[] = [
-                'origin' => $startingPoint,
-                'destination' => $currentDestination
-            ];
+            $currentDestination = $dataStructure[$startingPoint]['destination'];
+            $sortedCards[] = $dataStructure[$startingPoint]['board'];
             $startingPoint = $currentDestination;
         }
         return $sortedCards;
     }
 }
-
-
